75f421186efd24a77e11795c1eea915b62ea23b8
Steven <sjc@ualberta.ca>
2018-04-05 13:19:03 -0600

    Separated functionality for button pressing and button lighting

--- script.js
+++ script.js
@@ -2,7 +2,69 @@ let audioGreen = document.getElementsByTagName('audio')[0];
 let audioRed = document.getElementsByTagName('audio')[1];
 let audioYellow = document.getElementsByTagName('audio')[2];
 let audioBlue = document.getElementsByTagName('audio')[3];
-//let midpoint = (element.offsetWidth + element.style.getPropertyValue('border-left-width')*2)/2;
+
+let clickable = true;
+const USER_CLICKED = true;
+
+function clickGreen() {
+    clickable = false;
+    lightUpGreen(USER_CLICKED);
+}
+function clickRed() {
+    clickable = false;
+    lightUpRed(USER_CLICKED);
+}
+function clickYellow() {
+    clickable = false;
+    lightUpYellow(USER_CLICKED);
+}
+function clickBlue() {
+    clickable = false;
+    lightUpBlue(USER_CLICKED);
+}
+
+function lightUpGreen(userClicked) {
+    document.getElementById('all-buttons').style.borderLeftColor = 'green';
+    audioGreen.play();
+    setTimeout(() => {
+        if (userClicked) {
+            clickable = true;
+        }
+        document.getElementById('all-buttons').style.borderLeftColor = 'darkgreen';
+    }, 500);
+}
+function lightUpRed(userClicked) {
+    document.getElementById('all-buttons').style.borderTopColor = 'red';
+    audioRed.play();
+    setTimeout(() => {
+        if (userClicked) {
+            clickable = true;
+        }
+        document.getElementById('all-buttons').style.borderTopColor = 'darkred';
+    }, 500);
+}
+function lightUpYellow(userClicked) {
+    document.getElementById('all-buttons').style.borderBottomColor = 'yellow';
+    audioYellow.play();
+    setTimeout(() => {
+        if (userClicked) {
+            clickable = true;
+        }
+        document.getElementById('all-buttons').style.borderBottomColor = 'rgb(173, 173, 0)';    
+    }, 500);
+}
+function lightUpBlue(userClicked) {
+    document.getElementById('all-buttons').style.borderRightColor = 'blue'; 
+    audioBlue.play();
+    setTimeout(() => {
+        if (userClicked) {
+            clickable = true;
+        }
+        document.getElementById('all-buttons').style.borderRightColor = 'darkblue';
+    }, 500);
+}
+
+// Defining clickable area
 let borderWidth = getComputedStyle(document.getElementById('all-buttons'), null).getPropertyValue('border-left-width');
 borderWidth = Number(borderWidth.slice(0, borderWidth.length-2));
 let middleWidth = getComputedStyle(document.getElementById('middle-area'), null).getPropertyValue('width');
@@ -14,40 +76,37 @@ marginLeft = Number(marginLeft.slice(0, marginLeft.length-2));
 let midPointHorizontal = (borderWidth * 2 + middleWidth)/2 + marginLeft + 8;
 let midPointVertical = (borderWidth * 2 + middleWidth)/2 + marginTop + 8;
 
-
-document.getElementById('all-buttons').addEventListener('mousedown', (element) => {
+document.getElementById('all-buttons').addEventListener('click', (element) => {
     let x = element.clientX;
     let y = element.clientY;
     let distanceFromMiddle = Math.pow(
         Math.pow(element.clientX-midPointHorizontal,2)+
         Math.pow(element.clientY-midPointVertical,2),
     0.5);
-    console.log("x "+x);
-    console.log("y "+y);
-    if (distanceFromMiddle > middleWidth/2) {
+
+    if (distanceFromMiddle > middleWidth/2 && clickable) {
         if (element.clientX<midPointHorizontal && element.clientY<midPointVertical) {
-            document.getElementById('all-buttons').style.borderLeftColor = 'green';            
-            audioGreen.play();
+            clickGreen();
         }
         else if (element.clientX>midPointHorizontal && element.clientY<midPointVertical) {
-            document.getElementById('all-buttons').style.borderTopColor = 'red';            
-            audioRed.play();
+            clickRed();
         }
         else if (element.clientX<midPointHorizontal && element.clientY>midPointVertical) {
-            document.getElementById('all-buttons').style.borderBottomColor = 'yellow';            
-            audioYellow.play();
+            clickYellow();
         }
         else if (element.clientX>midPointHorizontal && element.clientY>midPointVertical) {
-            document.getElementById('all-buttons').style.borderRightColor = 'blue';            
-            audioBlue.play();
-        }
+            clickBlue();
+        }        
     }
 });
 
-document.getElementById('all-buttons').addEventListener('mouseup', () => {
-    document.getElementById('all-buttons').style.borderLeftColor = 'darkgreen';
-    document.getElementById('all-buttons').style.borderRightColor = 'darkblue';
-    document.getElementById('all-buttons').style.borderTopColor = 'darkred';
-    document.getElementById('all-buttons').style.borderBottomColor = 'rgb(173, 173, 0)';
-    
-});
+let buttonSeries = [];
+let level;
+
+function newGame() {
+    // Initialize with number from 0 - 3
+    buttonSeries = [Math.floor(Math.random() * 4)];
+    level = 1;
+}
+
+newGame();
