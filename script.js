10e124b66f5f5a5f7e270d374f508dbc4c29d51a
Steven <sjc@ualberta.ca>
2018-04-06 22:01:12 -0600

    Separate div into 4 divs for buttons

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
+//
+// Variables
+//
 let audioGreen = document.getElementsByTagName('audio')[0];
 let audioRed = document.getElementsByTagName('audio')[1];
 let audioYellow = document.getElementsByTagName('audio')[2];
 let audioBlue = document.getElementsByTagName('audio')[3];
 
-let clickable = true;
+let clickable = false;
 const USER_CLICKED = true;
 
+let buttonSeries = [];
+let myButtonPresses = [];
+let level;
+let strict = false;
+let strictGameOver = false;
+let timer;
+
+//
+// Functions
+//
 function clickGreen() {
+    console.log("clickGreen()");
     clickable = false;
+    clearTimeout(timer);
+    checkUserInput(0);
     lightUpGreen(USER_CLICKED);
 }
 function clickRed() {
+    console.log("clickRed()");
     clickable = false;
+    clearTimeout(timer);
+    checkUserInput(1);
     lightUpRed(USER_CLICKED);
 }
 function clickYellow() {
+    console.log("clickYellow()");
     clickable = false;
+    clearTimeout(timer);
+    checkUserInput(2);
     lightUpYellow(USER_CLICKED);
 }
 function clickBlue() {
+    console.log("clickBlue()");
     clickable = false;
+    clearTimeout(timer);
+    checkUserInput(3);
     lightUpBlue(USER_CLICKED);
 }
 
 function lightUpGreen(userClicked) {
+    console.log("lightUpGreen("+userClicked+")");
     document.getElementById('all-buttons').style.borderLeftColor = 'green';
     audioGreen.play();
     setTimeout(() => {
@@ -34,6 +60,7 @@ function lightUpGreen(userClicked) {
     }, 500);
 }
 function lightUpRed(userClicked) {
+    console.log("lightUpRed("+userClicked+")");
     document.getElementById('all-buttons').style.borderTopColor = 'red';
     audioRed.play();
     setTimeout(() => {
@@ -44,6 +71,7 @@ function lightUpRed(userClicked) {
     }, 500);
 }
 function lightUpYellow(userClicked) {
+    console.log("lightUpYellow("+userClicked+")");
     document.getElementById('all-buttons').style.borderBottomColor = 'yellow';
     audioYellow.play();
     setTimeout(() => {
@@ -54,6 +82,7 @@ function lightUpYellow(userClicked) {
     }, 500);
 }
 function lightUpBlue(userClicked) {
+    console.log("lightUpBlue("+userClicked+")");
     document.getElementById('all-buttons').style.borderRightColor = 'blue'; 
     audioBlue.play();
     setTimeout(() => {
@@ -64,25 +93,65 @@ function lightUpBlue(userClicked) {
     }, 500);
 }
 
-// Defining clickable area
-let borderWidth = getComputedStyle(document.getElementById('all-buttons'), null).getPropertyValue('border-left-width');
-borderWidth = Number(borderWidth.slice(0, borderWidth.length-2));
-let middleWidth = getComputedStyle(document.getElementById('middle-area'), null).getPropertyValue('width');
-middleWidth = Number(middleWidth.slice(0, middleWidth.length-2));
-let marginTop = getComputedStyle(document.getElementById('all-buttons'), null).getPropertyValue('margin-top');
-marginTop = Number(marginTop.slice(0, marginTop.length-2));
-let marginLeft = getComputedStyle(document.getElementById('all-buttons'), null).getPropertyValue('margin-left');
-marginLeft = Number(marginLeft.slice(0, marginLeft.length-2));
-let midPointHorizontal = (borderWidth * 2 + middleWidth)/2 + marginLeft + 8;
-let midPointVertical = (borderWidth * 2 + middleWidth)/2 + marginTop + 8;
+function newGame() {
+    console.log("newGame()");
+    // Initialize with number from 0 - 3
+    level = 1;
+    buttonSeries = [Math.floor(Math.random() * 4)];
+    myButtonPresses = [];  
+    showButtonSeries();
+    waitForResponse();
+}
 
-document.getElementById('all-buttons').addEventListener('click', (element) => {
-    let x = element.clientX;
-    let y = element.clientY;
-    let distanceFromMiddle = Math.pow(
-        Math.pow(element.clientX-midPointHorizontal,2)+
-        Math.pow(element.clientY-midPointVertical,2),
-    0.5);
+function showButtonSeries() {
+    console.log("showButtonSeries()");
+    console.log(buttonSeries);
+}
+
+function waitForResponse() {
+    console.log("waitForResponse()");
+    clickable = true;/*
+    timer = setTimeout(() => {
+        console.log('Timed out');
+    }, 5000);*/
+}
+
+function checkUserInput(num) {
+    console.log("checkUserInput()");
+    myButtonPresses.push(num);
+    if (num!==buttonSeries[myButtonPresses.length-1]) {
+        console.log('Wrong answer!');
+    }
+    else {
+        console.log('Right answer!');
+        nextStep();
+    }
+}
+
+function nextStep() {
+    console.log("nextStep()");
+    if (myButtonPresses.length === buttonSeries.length) {
+        if (level === 5) {
+            console.log("You win");
+        }
+        else {
+            level++;
+            buttonSeries.push(Math.floor(Math.random() * 4));
+            console.log("Level: "+level);
+            myButtonPresses = [];
+            showButtonSeries();
+        }
+    }
+    clickable = true;
+    waitForResponse();
+}
+
+//
+// Inputs
+//
+
+
+console.log(clientRects);
 
     if (distanceFromMiddle > middleWidth/2 && clickable) {
         if (element.clientX<midPointHorizontal && element.clientY<midPointVertical) {
@@ -96,17 +165,59 @@ document.getElementById('all-buttons').addEventListener('click', (element) => {
         }
         else if (element.clientX>midPointHorizontal && element.clientY>midPointVertical) {
             clickBlue();
-        }        
+        }
     }
 });
+*/
+document.getElementById('new-game').addEventListener('click', () => {
+    newGame();
+});
 
-let buttonSeries = [];
-let level;
+console.log(getComputedStyle(document.getElementById('middle-area')));
+console.log(document.getElementById('middle-area').getClientRects());
 
-function newGame() {
-    // Initialize with number from 0 - 3
-    buttonSeries = [Math.floor(Math.random() * 4)];
-    level = 1;
-}
+/*
+document.getElementById('all-buttons').addEventListener('click', (element) => {
+    // Defining clickable area
+    /*let borderWidth = getComputedStyle(document.getElementById('all-buttons'), null).getPropertyValue('border-left-width');
+    borderWidth = Number(borderWidth.slice(0, borderWidth.length-2));
+    let middleWidth = getComputedStyle(document.getElementById('middle-area'), null).getPropertyValue('width');
+    middleWidth = Number(middleWidth.slice(0, middleWidth.length-2));
+    let marginTop = getComputedStyle(document.getElementById('all-buttons'), null).getPropertyValue('margin-top');
+    marginTop = Number(marginTop.slice(0, marginTop.length-2));
+    let marginLeft = getComputedStyle(document.getElementById('all-buttons'), null).getPropertyValue('margin-left');
+    marginLeft = Number(marginLeft.slice(0, marginLeft.length-2));
+    let allMargins = getComputedStyle(document.getElementById('all-buttons'), null).getPropertyValue('margin');
+    let midPointHorizontal = (borderWidth * 2 + middleWidth)/2 + marginLeft + 8;
+    let midPointVertical = (borderWidth * 2 + middleWidth)/2 + marginTop + 8;
 
-newGame();
+    let borderWidth = getComputedStyle(document.getElementById('all-buttons'), null).getPropertyValue('border-left-width');
+    borderWidth = Number(borderWidth.slice(0, borderWidth.length-2));
+    let middleWidth = getComputedStyle(document.getElementById('middle-area'), null).getPropertyValue('width');
+    middleWidth = Number(middleWidth.slice(0, middleWidth.length-2));
+
+    let clientRects = document.getElementById('all-buttons').getClientRects();
+    let marginTop = clientRects[0].top;
+    let marginLeft = clientRects[0].left;
+
+    let midPointHorizontal = (borderWidth * 2 + middleWidth)/2 + marginLeft + 8;
+    let midPointVertical = (borderWidth * 2 + middleWidth)/2 + marginTop + 8;
+
+    let x = element.clientX;
+    let y = element.clientY;
+    let distanceFromMiddle = Math.pow(
+        Math.pow(element.clientX-midPointHorizontal,2)+
+        Math.pow(element.clientY-midPointVertical,2),
+    0.5);
+
+/*
+    console.log('borderWidth '+borderWidth);
+    console.log('middleWidth '+middleWidth);
+    console.log('marginTop '+marginTop);
+    console.log('marginLeft '+marginLeft);
+    console.log('midPointHorizontal '+midPointHorizontal);
+    console.log('midPointVertical '+midPointVertical);
+    console.log("x: "+x+". y: "+y);/*
+    console.log("distance from middle: "+distanceFromMiddle);
+    console.log("middle radius: "+middleWidth/2);    
+*/
